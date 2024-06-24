cb51ad5b1cb72a2783001f11caf530ce2b7e54ca
Kimani Vincent <kimanivincent2006@gmail.com>
2024-06-24 18:54:12 +0300

    test

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ async def main():
     # Load the flow from the file
     flow = AsyncFlows.from_file("hello_world.yaml")
     # Run the flow with a variable
-    result = await flow.set_vars(name="Kevin").run()
+    result = await flow.set_vars(name="Vincent").run()
     # Print the result
     print(result)
 
